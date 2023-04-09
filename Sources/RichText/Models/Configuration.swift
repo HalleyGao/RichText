0d7a2181d5bd6ffc9605ec5e4f9ea7172ec7e539
Halley Gao <hi@coding.so>
2023-04-09 13:24:28 +0800

    Update Configuration.swift

--- Sources/RichText/Models/Configuration.swift
+++ Sources/RichText/Models/Configuration.swift
@@ -30,7 +30,7 @@ public struct Configuration {
         customCSS: String = "",
         fontType: FontType = .system,
         fontColor: ColorSet = .init(light: "000000", dark: "F2F2F2"),
-        lineHeight: CGFloat = 170,
+        lineHeight: CGFloat = 30,
         colorScheme: ColorScheme = .auto,
         imageRadius: CGFloat = 0,
         linkOpenType: LinkOpenType = .SFSafariView(),
@@ -53,7 +53,7 @@ public struct Configuration {
     func css(isLight: Bool, alignment: TextAlignment) -> String {
         """
         img{max-height: 100%; min-height: 100%; height:auto; max-width: 100%; width:auto;margin-bottom:5px; border-radius: \(imageRadius)px;}
-        h1, h2, h3, h4, h5, h6, p, div, dl, ol, ul, pre, blockquote {text-align:\(alignment.htmlDescription); line-height: \(lineHeight)%; font-family: '\(fontType.name)' !important; color: \(fontColor.value(isLight)); }
+        h1, h2, h3, h4, h5, h6, p, div, dl, ol, ul, pre, blockquote {text-align:\(alignment.htmlDescription); line-height: \(lineHeight)px; font-family: '\(fontType.name)' !important; color: \(fontColor.value(isLight)); }
         iframe{width:100%; height:250px;}
         a:link {color: \(linkColor.value(isLight));}
         A {text-decoration: none;}
